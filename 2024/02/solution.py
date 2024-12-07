56034ebc3157e58ae898482b2c77b75e2461eea5
Daylen Pacheco <pacheco.daylen@gmail.com>
2024-12-06 23:31:16 -0700

    2024 Day 2

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -1,36 +1,37 @@
 from aoc_base import AOC
 
+def is_safe(report):
+    levels = list(map(int, report.split()))
+    n = len(levels)
+    differences = [levels[i + 1] - levels[i] for i in range(n - 1)]
+
+    is_increasing = all(0 < diff <= 3 for diff in differences)
+    is_decreasing = all(-3 <= diff < 0 for diff in differences)
+
+    valid_differences = all(-3 <= diff <= 3 and diff != 0 for diff in differences)
+    return (is_increasing or is_decreasing) and valid_differences
+
+def is_safe_with_dampener(report):
+    if is_safe(report):
+        return True
+
+    levels = list(map(int, report.split()))
+    for i in range(len(levels)):
+        modified_report = levels[:i] + levels[i + 1:]
+        if is_safe(" ".join(map(str, modified_report))):
+            return True
+
+    return False
+
 class Day02(AOC):
     def part1(self, data: str) -> any:
-        data_list = data.strip().splitlines()
-        reports = []
-        for report in data_list:
-            reports.append(report.split())
-
-        def check_is_safe(report_to_check):
-            differences = [abs(int(report[i]) - int(report[i + 1])) for i in range(len(report) - 1)]
-            if not all(1 <= diff <= 3 for diff in differences):
-                return False
-
-            increasing = all(report[i] < report[i + 1] for i in range(len(report) - 1))
-            decreasing = all(report[i] > report[i + 1] for i in range(len(report) - 1))
-
-            return increasing or decreasing
-
-
-
-        count = 0
-        for report in reports:
-            if check_is_safe(report):
-                count += 1
-            else:
-                pass
-
-        return count
+        reports = data.strip().split("\n")
+        return sum(is_safe(report) for report in reports)
 
 
     def part2(self, data) -> any:
-        pass
+        reports = data.strip().split("\n")
+        return sum(is_safe_with_dampener(report) for report in reports)
 
 
 sample_input = '''7 6 4 2 1
@@ -41,7 +42,7 @@ sample_input = '''7 6 4 2 1
 1 3 6 7 9
 '''
 expected_part1 = 2
-expected_part2 = None
+expected_part2 = 4
 Day02(
     year=2024,
     day=2,
